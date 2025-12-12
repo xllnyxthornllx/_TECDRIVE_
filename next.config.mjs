aaab9f8bf45f89a0537749168802d4800273f0c4
xllnyxthornllx <xragnard@gmail.com>
2025-12-12 11:32:23 -0500

    Config: Add next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // If you have images from external domains, configure them here.
+  // Example:
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'example.com',
+  //       port: '',
+  //       pathname: '/images/**',
+  //     },
+  //   ],
+  // },
+};
+
+export default nextConfig;
